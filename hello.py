943b742242de1a9d5c2f47a40a1b948d351b2808
jhat3 <31477572+jhat3@users.noreply.github.com>
2017-08-30 09:19:35 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+# This program says hello and greets a person by name.
+#
+# Justin Hatley
+# August 24, 2017
+
+print("Hello.")
+print("What is your name?")
+name = input()
+print("It is good to meet you, " + name + ".")
+
+
---
+++ madlib.py
@@ -0,0 +1,37 @@
+#Adlib
+
+print("Choose a noun.")
+noun1=input()
+
+print("Choose a adjective.")
+adjective1=input()
+
+print("Choose a name.")
+name1=input()
+
+print("Choose a number.")
+age1=input()
+
+print("Choose a place(non specific).")
+place1=input()
+
+print("Choose a name.")
+name2=input()
+
+print("Choose a car(specific).")
+car=input()
+
+print("Choose a number(1-31).")
+age2=input()
+
+print("Choose a month.")
+month=input()
+
+print("My name is "  + Name1 + " and I am " + age1 + " years old. Once I " +
+      "went to the store and bought a " + noun1 + " , it was very " + adjective1 +
+      ". I took this thing everywhere, even when I went to the " + place1 + "." +
+      "I was obsessed with it so much I called it " + name2 + ". But sadly, " +
+      "in " + month + " on the " + age2 + ", " + name2 + " passed away." +
+      "He was hit by a " + car + ".")
+
+    
